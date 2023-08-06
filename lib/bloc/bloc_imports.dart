a3950fdca5ec44a5d3be0d50001f843c78e7a408
Jaison Dias <jaisondias1994@gmail.com>
2023-08-06 22:11:22 +0530

    Bloc Counter App

---
+++ lib/bloc/bloc_imports.dart
@@ -0,0 +1,5 @@
+export "package:flutter_bloc/flutter_bloc.dart";
+export 'counter/counter_bloc.dart';
+export 'package:app_bloc/bloc/counter/counter_bloc.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export './my_bloc_observer.dart';
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:app_bloc/screens/counter.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import '../bloc/bloc_imports.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
